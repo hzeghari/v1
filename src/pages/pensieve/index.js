4e7ed7d4a2ce420acc8048efd0d61774cebfbe70
hamza-zeghari <zeghari.hamzaa@gmail.com>
2020-02-09 10:44:54 +0100

    fix

--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -123,7 +123,7 @@ const PensievePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Pensieve | Brittany Chiang</title>
+        <title>Pensieve | Hamza Zeghari</title>
         <link rel="canonical" href="https://brittanychiang.com/pensieve" />
       </Helmet>
 
